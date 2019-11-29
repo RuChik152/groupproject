2ba42df3b22266db7c7a09dd5df4da5198232a07
RuChik152 <57956402+RuChik152@users.noreply.github.com>
2019-11-29 16:53:08 +0300

    basket-pay

--- src/js/sum.js
+++
@@ -1,9 +0,0 @@
-function sums()
-{
-    sum = 0;
-    for (i=1; i<2; i++) {
-        var elem = document.getElementById("r" + i);
-        if(elem)sum = sum + parseInt(elem.value);
-    }
-    dicument..content_basket_panel_item_panel.sumOut.value=sum
-}
